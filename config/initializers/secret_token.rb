aae14f31089f8334c6642b42c2cfc6bc5cf7f52b
RaymondChou <freezestart@gmail.com>
2014-07-09 16:12:45 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+THXLWeChat::Application.config.secret_token = '4fb913d115d25de3eaa79c7c8f9cf33290094c8bcee9a41b761b88972dddd687ef8ff51eb3d9db04d0215799bf9f64fe5badf5631c2c7835683bf2ef907bea38'
